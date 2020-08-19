eba1becc81e5162ef698c4049f256221b8d718ac
Neverender <tldavis09@yahoo.com>
2020-08-18 20:01:44 -0500

    Updates

---
+++ precache-manifest.2384ad5473c89f6c751f0deb3bdd6cc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "960cae6b65c50af6afabb9039dafc2ac",
+    "url": "/portfolio-2020/index.html"
+  },
+  {
+    "revision": "6a39732b4c942e1d7298",
+    "url": "/portfolio-2020/static/css/main.14be8f96.chunk.css"
+  },
+  {
+    "revision": "57421d09f2aaac334323",
+    "url": "/portfolio-2020/static/js/2.341fbd2e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio-2020/static/js/2.341fbd2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a39732b4c942e1d7298",
+    "url": "/portfolio-2020/static/js/main.1a3be323.chunk.js"
+  },
+  {
+    "revision": "bbf3efb9002603ffad2a",
+    "url": "/portfolio-2020/static/js/runtime-main.106eb575.js"
+  }
+]);
