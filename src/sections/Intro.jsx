c932f2e25dd68042687d8719eba1aa3fddce500b
Neverender <tldavis09@yahoo.com>
2020-08-23 23:03:54 -0500

    minor change to about again

--- src/sections/Intro.jsx
+++ src/sections/Intro.jsx
@@ -18,8 +18,8 @@ const Intro = () => {
 				insurance fraud analysis and special investigations. With a
 				certificate in full stack development from Southern Methodist
 				University, my ambition is to construct engaging applications
-				that focus on the user first. An ardent love for aesthetic
-				design and a penchant for abstract problem-solving inspire
+				that focus on the user first. A penchant for abstract
+				problem-solving and an ardent love for aesthetic design inspire
 				continuous improvement of my proficiency as a developer.
 			</p>
 		</div>
