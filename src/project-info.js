0af566abc176ccbaad03146260e67508585bfb22
tldav <tldavis09@yahoo.com>
2021-09-15 11:33:29 -0500

    updated projects

--- src/project-info.js
+++ src/project-info.js
@@ -7,38 +7,38 @@ const projectInfo = [
 			"Check your Horoscope in Yoda Speech. Created using JavaScript, Tailwind CSS, and third party APIs: Aztro and Yodish.",
 		image: process.env.PUBLIC_URL + "/assets/space-420-278.jpg",
 		appUrl: "https://tldav.github.io/yoda-horoscope/",
-		repoUrl: "https://github.com/tldav/yoda-horoscope"
+		repoUrl: "https://github.com/tldav/yoda-horoscope",
 	},
 	{
 		id: 2,
-		name: "plumm.it",
+		name: "plummit",
 		css: "plumm-title",
 		description:
-			"A forum with user signup and authentication. Created using JavaScript, jQuery, Node, Express, and MySQL with Sequelize.",
+			"A forum with user signup and authentication. Created using React, Node.js, Express, MySQL, Passport, and Context API for state management. ",
 		image: process.env.PUBLIC_URL + "/assets/plum-420-278.jpg",
-		appUrl: "https://warm-plains-89008.herokuapp.com/logout",
-		repoUrl: "https://github.com/marcustate/project-purple/"
+		appUrl: "https://plummit.herokuapp.com/",
+		repoUrl: "https://github.com/tldav/plumm-it",
 	},
 	{
 		id: 3,
 		name: "Workout App",
 		css: "fit-title",
 		description:
-			"Create custom workouts and graph your progress. Created using JavaScript, Node, Express, and MongoDB with Mongoose. ",
+			"Create custom workouts and graph your progress. Created using JavaScript, Node.js, Express, and MongoDB with Mongoose. ",
 		image: process.env.PUBLIC_URL + "/assets/fit-420-278.jpg",
 		appUrl: "https://boiling-hamlet-55992.herokuapp.com/",
-		repoUrl: "https://github.com/tldav/fitness-tracker"
+		repoUrl: "https://github.com/tldav/fitness-tracker",
 	},
 	{
 		id: 4,
 		name: "Charity Tracker",
 		css: "charity-title",
 		description:
-			'Manage non-profit organizations to partner with your business. Created with React, Material-UI, Node, Express, bcrypt, and MongoDB. Test using "example" as username and password.',
+			'Manage non-profit organizations to partner with your business. Created with React, Material-UI, Node.js, Express, and MongoDB. Test using "example" as username and password.',
 		image: process.env.PUBLIC_URL + "/assets/charity-420-278.jpg",
 		appUrl: "https://charity-wowness.herokuapp.com/",
-		repoUrl: "https://github.com/ThomasJasonM1/charity-tracker"
-	}
+		repoUrl: "https://github.com/ThomasJasonM1/charity-tracker",
+	},
 ];
 
 export default projectInfo;
