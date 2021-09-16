c213718b456847695ede277dee2c1a8c01e8646b
tldav <tldavis09@yahoo.com>
2021-09-16 14:02:30 -0500

    update project info

--- src/project-info.js
+++ src/project-info.js
@@ -1,6 +1,16 @@
 const projectInfo = [
 	{
 		id: 1,
+		name: "plummit",
+		css: "plumm-title",
+		description:
+			"A social forum to post and engage in conversation. Created using React, Node.js, Express, MySQL, Passport, and Context API for state management. ",
+		image: process.env.PUBLIC_URL + "/assets/plum-420-278.jpg",
+		appUrl: "https://plummit.herokuapp.com/",
+		repoUrl: "https://github.com/tldav/plumm-it",
+	},
+	{
+		id: 2,
 		name: "YodaScope",
 		css: "yoda-title",
 		description:
@@ -10,27 +20,7 @@ const projectInfo = [
 		repoUrl: "https://github.com/tldav/yoda-horoscope",
 	},
 	{
-		id: 2,
-		name: "plummit",
-		css: "plumm-title",
-		description:
-			"A forum with user signup and authentication. Created using React, Node.js, Express, MySQL, Passport, and Context API for state management. ",
-		image: process.env.PUBLIC_URL + "/assets/plum-420-278.jpg",
-		appUrl: "https://plummit.herokuapp.com/",
-		repoUrl: "https://github.com/tldav/plumm-it",
-	},
-	{
 		id: 3,
-		name: "Workout App",
-		css: "fit-title",
-		description:
-			"Create custom workouts and graph your progress. Created using JavaScript, Node.js, Express, and MongoDB with Mongoose. ",
-		image: process.env.PUBLIC_URL + "/assets/fit-420-278.jpg",
-		appUrl: "https://boiling-hamlet-55992.herokuapp.com/",
-		repoUrl: "https://github.com/tldav/fitness-tracker",
-	},
-	{
-		id: 4,
 		name: "Charity Tracker",
 		css: "charity-title",
 		description:
@@ -38,6 +28,16 @@ const projectInfo = [
 		image: process.env.PUBLIC_URL + "/assets/charity-420-278.jpg",
 		appUrl: "https://charity-wowness.herokuapp.com/",
 		repoUrl: "https://github.com/ThomasJasonM1/charity-tracker",
+	},
+	{
+		id: 4,
+		name: "Workout App",
+		css: "fit-title",
+		description:
+			"Create custom workouts and graph your progress. Created using JavaScript, Node.js, Express, and MongoDB with Mongoose.",
+		image: process.env.PUBLIC_URL + "/assets/fit-420-278.jpg",
+		appUrl: "https://boiling-hamlet-55992.herokuapp.com/",
+		repoUrl: "https://github.com/tldav/fitness-tracker",
 	},
 ];
 
